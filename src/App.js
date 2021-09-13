7365586ad707a7ed5deef287c0bfdf23ae29523a
blahman <84323029+KKaciusis@users.noreply.github.com>
2021-09-13 15:26:09 +0300

    Sorta working drop down

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ function App() {
   return (
     <div className="App">
       <NavBar><NavItem navIcon="icon">
-        <p>Cows are literraly fart machines</p>
+        <DropDown></DropDown>
         </NavItem></NavBar>
       <Images />
     </div>
   );
-}
+};
+function DropDown(props){
+  function DropItems(props){
+    return (
+      <a href="#" className="dropItem">
+        <span className="dropButton">{props.leftIcon}</span>
+        {props.children}
+      </a>
+    );
+
+  };
+  return (
+    <div className="dropDown">
+      <DropItems><img src='icon/chewie.svg'/></DropItems>
+      <DropItems><img src='icon/plus.svg'/></DropItems>
+
+    </div>
+  );
+};
 
 export default App;
